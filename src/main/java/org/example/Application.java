e069342518aaf5f5195e2e9e746901ec697f3526
Mohammed Ali Baddah <mabaddah@gmail.com>
2023-01-29 18:30:52 +1100

    Fixed crashing on build

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,9 +2,8 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.service"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
